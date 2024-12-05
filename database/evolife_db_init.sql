9335d08fdeb42d91c8420f2c1cb2bf8579bb5843
Joseph Owiti <josephowiti7@gmail.com>
2024-12-05 10:38:17 -0500

    removing users table and foreign keys

--- database/evolife_db_init.sql
+++ database/evolife_db_init.sql
@@ -8,73 +8,73 @@ SHOW TABLES;
 SET GLOBAL local_infile = 1;
 
 
--- USERS TABLE
+-- -- USERS TABLE
 
-CREATE TABLE IF NOT EXISTS users (
-    user_id VARCHAR(30) PRIMARY KEY,
-    breq_amotivation FLOAT,
-    breq_external_regulation FLOAT,
-    breq_introjected_regulation FLOAT,
-    breq_identified_regulation FLOAT,
-    breq_intrinsic_regulation FLOAT,
-    breq_self_determination VARCHAR(50),
-    positive_affect_score FLOAT,
-    negative_affect_score FLOAT,
-    agreeableness FLOAT,
-    conscientiousness FLOAT,
-    stability FLOAT,
-    intellect FLOAT,
-    stai_stress FLOAT,
-    stai_stress_category VARCHAR(50),
-    ttm_consciousness_raising FLOAT,
-    ttm_dramatic_relief FLOAT,
-    ttm_environmental_reevaluation FLOAT,
-    ttm_self_reevaluation FLOAT,
-    ttm_social_liberation FLOAT,
-    ttm_counterconditioning FLOAT,
-    ttm_helping_relationships FLOAT,
-    ttm_reinforcement_management FLOAT,
-    ttm_self_liberation FLOAT,
-    ttm_stimulus_control FLOAT
-) ENGINE=InnoDB;
+-- CREATE TABLE IF NOT EXISTS users (
+--     user_id VARCHAR(30) PRIMARY KEY,
+--     breq_amotivation FLOAT,
+--     breq_external_regulation FLOAT,
+--     breq_introjected_regulation FLOAT,
+--     breq_identified_regulation FLOAT,
+--     breq_intrinsic_regulation FLOAT,
+--     breq_self_determination VARCHAR(50),
+--     positive_affect_score FLOAT,
+--     negative_affect_score FLOAT,
+--     agreeableness FLOAT,
+--     conscientiousness FLOAT,
+--     stability FLOAT,
+--     intellect FLOAT,
+--     stai_stress FLOAT,
+--     stai_stress_category VARCHAR(50),
+--     ttm_consciousness_raising FLOAT,
+--     ttm_dramatic_relief FLOAT,
+--     ttm_environmental_reevaluation FLOAT,
+--     ttm_self_reevaluation FLOAT,
+--     ttm_social_liberation FLOAT,
+--     ttm_counterconditioning FLOAT,
+--     ttm_helping_relationships FLOAT,
+--     ttm_reinforcement_management FLOAT,
+--     ttm_self_liberation FLOAT,
+--     ttm_stimulus_control FLOAT
+-- ) ENGINE=InnoDB;
 
--- load initial Kaggle individual data into our users table
+-- -- load initial Kaggle individual data into our users table
 
-LOAD DATA INFILE '/var/lib/mysql-files/database_individuals.csv'
-INTO TABLE users
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(
-    @dummy,
-    user_id, 
-    breq_amotivation,
-    breq_external_regulation,
-    breq_introjected_regulation,
-    breq_identified_regulation,
-    breq_intrinsic_regulation,
-    breq_self_determination,
-    positive_affect_score,
-    negative_affect_score,
-    agreeableness,
-    conscientiousness,
-    stability,
-    intellect,
-    stai_stress,
-    stai_stress_category,
-    ttm_consciousness_raising,
-    ttm_dramatic_relief,
-    ttm_environmental_reevaluation,
-    ttm_self_reevaluation,
-    ttm_social_liberation,
-    ttm_counterconditioning,
-    ttm_helping_relationships,
-    ttm_reinforcement_management,
-    ttm_self_liberation,
-    ttm_stimulus_control
-);
+-- LOAD DATA INFILE '/var/lib/mysql-files/database_individuals.csv'
+-- INTO TABLE users
+-- FIELDS TERMINATED BY ','
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS
+-- (
+--     @dummy,
+--     user_id, 
+--     breq_amotivation,
+--     breq_external_regulation,
+--     breq_introjected_regulation,
+--     breq_identified_regulation,
+--     breq_intrinsic_regulation,
+--     breq_self_determination,
+--     positive_affect_score,
+--     negative_affect_score,
+--     agreeableness,
+--     conscientiousness,
+--     stability,
+--     intellect,
+--     stai_stress,
+--     stai_stress_category,
+--     ttm_consciousness_raising,
+--     ttm_dramatic_relief,
+--     ttm_environmental_reevaluation,
+--     ttm_self_reevaluation,
+--     ttm_social_liberation,
+--     ttm_counterconditioning,
+--     ttm_helping_relationships,
+--     ttm_reinforcement_management,
+--     ttm_self_liberation,
+--     ttm_stimulus_control
+-- );
 
--- END USERS TABLE
+-- -- END USERS TABLE
 
 -- HEALTH METRICTS TABLE
 
@@ -111,8 +111,7 @@ CREATE TABLE IF NOT EXISTS health_metrics (
     day INT,
     pred_bmi VARCHAR(10) DEFAULT NULL,
     pred_tense_anxious BOOLEAN DEFAULT NULL,
-    pred_tired BOOLEAN DEFAULT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    pred_tired BOOLEAN DEFAULT NULL
 ) ENGINE=InnoDB;
 
 
@@ -166,8 +165,7 @@ CREATE TABLE IF NOT EXISTS encoded_health_metrics (
     encoding_id INT PRIMARY KEY,
     user_id VARCHAR(30),
     day INT,
-    encoding VARCHAR(75),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    encoding VARCHAR(75)
 ) ENGINE=InnoDB;
 
 LOAD DATA INFILE '/var/lib/mysql-files/encodings.csv'
@@ -178,7 +176,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (
     encoding_id, user_id, day, encoding
-)
+);
 
 -- END ENCODINGS TABLE
 
@@ -190,18 +188,17 @@ CREATE TABLE IF NOT EXISTS phenotype_data (
     month_1 FLOAT,
     month_2 FLOAT,
     month_3 FLOAT,
-    month_4 FLOAT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    month_4 FLOAT
 ) ENGINE=InnoDB;
 
 LOAD DATA INFILE '/var/lib/mysql-files/phenotype_data.csv'
-INTO TABLE health_metrics
+INTO TABLE phenotype_data
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (
     pheno_id, user_id, month_1, month_2, month_3, month_4
-)
+);
 
 -- END PHENOTYPE TABLE
