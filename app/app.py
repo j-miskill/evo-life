a1e50ef44a4d48d4eadd189f91a2f60ba2e891d8
Joseph Owiti <josephowiti7@gmail.com>
2024-12-11 00:23:41 -0500

    added phenotype trends chart

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import mysql.connector
 # from trigger_pipeline import trigger_inference
 from db_connector import get_db_connection
 import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -38,15 +39,18 @@ def get_metrics(user_id, month):
     conn.close()
     return jsonify(health_metrics)
 
-
 @app.route("/get_encodings/<user_id>/<month>")
 def get_encodings(user_id, month):
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT encoding FROM encoded_health_metrics WHERE (user_id = %s AND day IN (SELECT day FROM health_metrics WHERE month(date) = %s))", (user_id, month))
+    cursor.execute("SELECT *, REPLACE(REPLACE(encoding,'[',''),']','') as encoding_stripped FROM encoded_health_metrics WHERE (user_id = %s AND day IN (SELECT day FROM health_metrics WHERE month(date) = %s))", (user_id, month))
     health_encodings = cursor.fetchall()
     conn.close()
-    return jsonify(health_encodings)
+    health_encodings = pd.DataFrame(health_encodings)
+    health_encodings['encoding_sum'] = health_encodings['encoding_stripped'].apply(lambda cell: sum(map(int, cell.split())))
+    health_encodings_dict = health_encodings.to_dict(orient='records')
+
+    return jsonify(health_encodings_dict)
 
 @app.route("/get_phenotypes/<user_id>/<month>")
 def get_phenotype(user_id, month):
@@ -77,6 +81,28 @@ def get_metric_trends(user_id):
     conn.close()
     
     return jsonify(health_metric_trends)
+
+@app.route("/get_phenotype_trends/<user_id>")
+def get_phenotype_trends(user_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    
+    # Fetch data for the last three months
+    query = """
+        SELECT month, phenotype_score FROM (
+            SELECT * FROM phenotype_data ph
+            WHERE ph.user_id = %s
+            ORDER BY ph.month DESC
+            LIMIT 12
+        ) nph
+        ORDER BY nph.month ASC;
+
+    """
+    cursor.execute(query, (user_id,))
+    phenotype_trends = cursor.fetchall()
+    conn.close()
+    
+    return jsonify(phenotype_trends)
 
 
 if __name__ == "__main__":
